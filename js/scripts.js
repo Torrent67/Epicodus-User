669662d86168f2744e6b9e5ea4738a85286c4afa
Marqis Ramos and Trevor Teigen <trevor@>
2019-06-03 16:30:49 -0700

    added reverse and second button

--- js/scripts.js
+++ js/scripts.js
@@ -11,12 +11,26 @@ $(document).ready(function(){
     var middle = midway (initial, midpointChar);
     var buttonOne = button1(middle,sentenceFL);
     var test = initial.charAt(midpointChar);
-    alert (firstLast);
-    alert (midpointChar);
+    var reversed = reverseString(buttonOne);
     alert (buttonOne);
     })
-})
 
+$("#click2").click(function(){
+  var initial = prompt ("Input random fact about self to generate code name.");
+  var letterCount = initial.length;
+  var firstChar = initial.charAt(0).toUpperCase();
+  var lastChar = initial.charAt(letterCount-1).toUpperCase();
+  var firstLast = capsFirst(firstChar, lastChar);
+  var lastFirst = capsLast(lastChar, firstChar);
+  var sentenceFL = initfl(initial, firstLast);
+  var midpointChar = (letterCount/2);
+  var middle = midway (initial, midpointChar);
+  var buttonOne = button1(middle,sentenceFL);
+  var test = initial.charAt(midpointChar);
+  var reversed = reverseString(buttonOne);
+  alert (reversed);
+  })
+})
 var capsFirst = function(firstChar, lastChar) {
   return firstChar+lastChar;
 }
@@ -31,4 +45,7 @@ var midway = function (initial, midpointChar)  {
 }
 var button1 = function (middle, sentenceFL) {
   return middle+sentenceFL;
+}
+var reverseString = function (buttonOne) {
+  return buttonOne.split("").reverse().join("");
 }

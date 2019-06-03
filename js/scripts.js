28b978e671f89b93625082d1c44e00b219929493
Marqis Ramos and Trevor Teigen <marqis@>
2019-06-03 14:47:34 -0700

    got it working

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
   $("#click").click(function(){
-    var initial= prompt ("Tell me about yourself. Age, height, favorite pet, name of first grade teacher.");
-    return initial;
-    var firstLetter=initial.charAt(0);
-    return firstLetter;
-    alert (firstLetter);
+    var initial = prompt ("Tell me about yourself. Age, height, favorite pet, name of first grade teacher.");
+      var letterCount = initial.length;
+      var firstChar = initial.charAt(0).toUpperCase();
+      var lastChar = initial.charAt(letterCount-1).toUpperCase();
+      alert (firstChar+lastChar);
+      var reverse = lastChar+firstChar;
+      alert (reverse);
+
+
+
   })
 })

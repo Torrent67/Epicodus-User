3df5008a182bdb5f24412275ce61a49d0bc65d2c
Marqis Ramos and Trevor Teigen <marqis@>
2019-06-03 14:24:00 -0700

    setting up initial function

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
-  $(#click).click(function(){
-    
+  $("#click").click(function(){
+    var initial= prompt ("Tell me about yourself. Age, height, favorite pet, name of first grade teacher.");
+    return initial;
+    var firstLetter=initial.charAt(0);
+    return firstLetter;
+    alert (firstLetter);
   })
-}
+})

7b05e6876f4612c48dd7f3c9434c224019b77e91
Marqis Ramos and Trevor Teigen <trevor@>
2019-06-03 15:18:36 -0700

    completed three functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,24 @@
 $(document).ready(function(){
   $("#click").click(function(){
     var initial = prompt ("Tell me about yourself. Age, height, favorite pet, name of first grade teacher.");
-      var letterCount = initial.length;
-      var firstChar = initial.charAt(0).toUpperCase();
-      var lastChar = initial.charAt(letterCount-1).toUpperCase();
-      alert (firstChar+lastChar);
-      var reverse = lastChar+firstChar;
-      alert (reverse);
-
-
-
-  })
+    var letterCount = initial.length;
+    var firstChar = initial.charAt(0).toUpperCase();
+    var lastChar = initial.charAt(letterCount-1).toUpperCase();
+    var firstLast = capsFirst(firstChar, lastChar);
+    var lastFirst = capsLast(lastChar, firstChar);
+    var comboCaps = combined(firstLast,lastFirst);
+    alert (firstLast);
+    alert (lastFirst);
+    alert (comboCaps);
+    })
 })
+
+var capsFirst = function(firstChar, lastChar) {
+  return firstChar+lastChar;
+}
+var capsLast = function(lastChar, firstChar) {
+  return lastChar+firstChar;
+}
+var combined = function (firstLast, lastFirst) {
+  return firstLast + lastFirst;
+}
